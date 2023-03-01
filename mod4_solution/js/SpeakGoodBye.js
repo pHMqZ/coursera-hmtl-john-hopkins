790e77d343e7fe997b43f6a85f60238aae20f3de
Phillip Marques <phillip.silva@capgemini.com>
2023-03-01 17:09:25 -0300

    Assignment Module 4

---
+++ mod4_solution/js/SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function(window){
+    var speakWord = "GoodBye ";
+    var byeSpeaker = function (name){
+        console.log(speakWord + name);
+    }
+
+    window.byeSpeaker = byeSpeaker;
+})(window);
