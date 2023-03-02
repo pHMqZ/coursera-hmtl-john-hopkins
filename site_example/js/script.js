35097ce59e51d6964ba9caafbdc728f1ac0b96f4
Phillip Marques <phillipmarq@hotmail.com>
2023-03-01 22:06:48 -0300

    Dynamically Loading Home View Content

--- site_example/js/script.js
+++ site_example/js/script.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function(){ 
 
 $("#navbarToggle").blur(function (event) { 
     var screenWidth = window.innerWidth;
@@ -7,3 +7,32 @@ $("#navbarToggle").blur(function (event) {
     }
  });
 });
+
+(function(global){
+    var dc = {};
+
+    var homeHtml = "snippets/home-snippet.html";
+
+    var insertHtml = function (selector, html){
+        var targetElem = document.querySelector(selector);
+        targetElem.innerHTML = html;
+    }
+
+    var showLoading = function(selector) {
+        var html = "<div class= 'text-center'>";
+        html += "<img src='images/ajax-loader.gif'></div>";
+        insertHtml(selector, html);
+    }
+
+    document.addEventListener("DOMContentLoaded", function(event) {
+        showLoading("#main-content");
+        $ajaxUtils.sendGetRequest(
+            homeHtml,
+            function(responseText){
+                document.querySelector("#main-content").innerHTML = responseText;
+            },
+        false);
+    });
+
+    global.$dc = dc;
+})(window);
